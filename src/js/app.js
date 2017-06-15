59794da0edec00e5a6515dc79445dfd57bb697f5
Helga13 <oliatrubina@yandex.ru>
2017-06-15 17:06:26 +0300

    all pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 	
+	function audioInit(){
+		$('audio').mediaelementplayer({
+			alwaysShowControls: true,
+			videoVolume: 'horizontal',
+			enableAutosize: true,
+			videoVolume: 'vertical'
+		});
+	}
+	
 	// menu border
 	
 	var style = 'easeOutExpo';
@@ -20,5 +29,76 @@ $(document).ready(function () {
 			default_width = $('.menu ul li.active').outerWidth();
 			$('#border').stop(false, true).animate({left: default_left, width: default_width},{duration:1500, easing: style});	
 		});
+	
+	// tabs
+	$('.tabs').each(function () {
+		$(this).find('.tab').each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();	$(this).addClass('active').siblings().removeClass('active').closest('.tabs').find('>.tabs_content').removeClass('active').eq(i).addClass('active');
+				
+				//video
+				var myPlayer = $('.video');  
+				var w = parseInt($('.video').css('width'));
+				var h = parseInt($('.video').css('height')) ;
+
+				$('video').mediaelementplayer({
+					alwaysShowControls: true,
+					videoVolume: 'horizontal',
+					defaultVideoWidth: w ,
+					defaultVideoHeight: h,
+					// if set, overrides <video width>
+					videoWidth: -1,
+					// if set, overrides <video height>
+					videoHeight: -1,
+					enableAutosize: true,
+					videoVolume: 'vertical'
+				});
+				//audio
+				audioInit();
+			});
+		});
+	});
+	
+	if('')
+	
+	$('.tab.single').on('click', function (){
+		audioInit();
+	});
+	
+	// datapicker
+	
+	$('.date').datepicker({
+		dateFormat:'dd-mm-y',
+		showOn:"both",
+		buttonImage:"img/icons/icon_calendar.png",
+		showOtherMonths:true, 
+		selectOtherMonths:true
+	});
+	
+	// video
+	
+//	 	var myPlayer = $('.video');  
+//
+//    var w = parseInt($('.video').css('width'));
+//    var h = parseInt($('.video').css('height')) ;
+//	
+//	$('video').mediaelementplayer({
+//    alwaysShowControls: true,
+//    videoVolume: 'horizontal',
+//		defaultVideoWidth: w ,
+//    defaultVideoHeight: h,
+//		// if set, overrides <video width>
+//    videoWidth: -1,
+//    // if set, overrides <video height>
+//    videoHeight: -1,
+//		enableAutosize: true,
+//		videoVolume: 'vertical'
+//  });
+	
+	//audio
+	
+	audioInit();
+				
+
 	
 })
--- src/js/common.js
+++ src/js/common.js
@@ -2,5 +2,7 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= lib/jquery.easing.1.3.js
 //= lib/salvattore.min.js
+//= lib/jquery-ui.min.js
+//= lib/mediaelement-and-player.min.js
 
 //= app.js
